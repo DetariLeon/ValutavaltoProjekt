db38c56f1444aeb8286562f331e970c5f14680fe
DetariLeon <detari.leon.2020i@bankitatabanya.hu>
2023-01-10 11:35:07 +0100

    Kész feladat

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
 function valutavalto() {
-    var huf = document.getElementById("forint").value;
+    let huf = document.getElementById("forint").value;
     let eur = 404
     let usd = 376.48
     let yen = 2.88
-    
-    document.getElementById('eur').value = (huf / eur).toFixed(2)
-    document.getElementById('usd').value = (huf / usd).toFixed(2)
-    document.getElementById('yen').value = (huf / yen).toFixed(2)
+    document.getElementById('eur').innerHTML = (huf / eur).toFixed(2) + " EUR"
+    document.getElementById('usd').innerHTML = (huf / usd).toFixed(2) + " USD"
+    document.getElementById('yen').innerHTML = (huf / yen).toFixed(2) + " JPY"
     return false
-}
+  }
